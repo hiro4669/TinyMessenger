3af82e44c542a00b6afbf9094bad8d3c5e9dc020
Hiroaki Fukuda <hiroaki@java2.jp>
2017-08-25 08:23:32 +0700

    move textField

--- TinyMessenger/ViewController.swift
+++ TinyMessenger/ViewController.swift
@@ -13,9 +13,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITextFieldDelega
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageField: UITextField!
     
-//    var items = [String]()
-    
+
     var messages = [Message]()
+    
+    var editingField:UITextField?
+    var overlap:CGFloat = 0.0
     
     func update() -> Void {
         let nos = tableView.numberOfSections
@@ -59,6 +61,23 @@ class ViewController: UIViewController, UITableViewDataSource, UITextFieldDelega
         initTextField()
         
         
+        let notification = NotificationCenter.default
+        
+        // keyboard change
+        notification.addObserver(self,
+                                 selector: #selector(self.keyboardChangeFrame(_:)),
+                                 name: NSNotification.Name.UIKeyboardDidChangeFrame, object: nil)
+        
+        // keyboard appear
+        notification.addObserver(self,
+                                 selector: #selector(self.keyboardWillShow(_:)),
+                                 name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+        // keyboard hide
+        notification.addObserver(self,
+                                 selector: #selector(self.keyboardDidHide(_:)), name: Notification.Name.UIKeyboardDidHide,
+                                 object: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -89,10 +108,65 @@ class ViewController: UIViewController, UITableViewDataSource, UITextFieldDelega
         return cell
     }
     
+    /* textField Delegate */
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("didBeginEditing")
+        self.editingField = textField
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("didEndEditing")
+        self.editingField = nil
+    }
+    
     public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         view.endEditing(true)
         return false
     }
+    
+    
+    /* Notification Center */
+    func keyboardChangeFrame(_ notification: Notification) -> Void {
+        print("keyboard change frame")
+        guard let fld = editingField else {
+            return
+        }
+//        print(fld)
+        let userInfo = (notification as Notification).userInfo!
+        let keyboardFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let newY = keyboardFrame.minY - fld.frame.size.height
+        let newFrame = CGRect(x: fld.frame.minX, y: newY, width: fld.frame.size.width, height: fld.frame.size.height)
+        fld.frame = newFrame
+        /*
+        let userInfo = (notification as Notification).userInfo!
+        let keyboardFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        print("field = \(fld.frame.maxY)")
+        print("keyboard min = \(keyboardFrame.minY)")
+        print("keyboard max = \(keyboardFrame.maxY)")
+        print("text height = \(fld.frame.size.height)")
+        
+        overlap = fld.frame.maxY - keyboardFrame.midY + 5
+        if overlap > 0 {
+            print("overlap = \(overlap)")
+            let newY = keyboardFrame.minY - fld.frame.size.height
+
+            let newFrame = CGRect(x: fld.frame.minX, y: newY, width: fld.frame.size.width, height: fld.frame.size.height)
+//            let newFrame = CGRect(x: fld.frame.minX, y: overlap, width: fld.frame.size.width, height: fld.frame.size.height)
+            fld.frame = newFrame
+        }
+        */
+        
+    }
+    
+    func keyboardWillShow(_ notification: Notification) {
+        print("keyboard will show")
+    }
+    
+    func keyboardDidHide(_ notification: Notification) {
+        print("keyboard will hide")
+    }
+    
+    
 
 }
 
